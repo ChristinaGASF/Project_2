ff3f38a1563235c706de0b3587552764b87cd71b
kleranscoding <clarencecc88@gmail.com>
2018-12-17 17:56:34 -0600

    modified index.html, corrected app.js, added youtube_categories.js

--- youtube-api-node/public/scripts/app.js
+++ youtube-api-node/public/scripts/app.js
@@ -1,39 +1,45 @@
 console.log('in-sanity check');
 
 var youtubeBaseURL= 'https://youtube.com/watch?v=';
-var part= 'snippet', regionCode='US', chart='mostPopular';
+var part= 'snippet,contentDetails,statistics', regionCode='US', chart='mostPopular';
+var order= 'viewCount';
 var maxResults= 10;
-var i= 0, maxResults= 20;
-var key= '{YOUR_API_KEY}';
-var baseURL= `https://www.googleapis.com/youtube/v3/search?part=${part}&regionCode=${regionCode}&maxResults=${maxResults}&key=${key}`;
+var i= 0, maxShowResults= 20;
+var key= 'REPLACE_API';
+var baseURL= `https://www.googleapis.com/youtube/v3/videos?part=${part}&regionCode=${regionCode}&maxResults=${maxResults}&order=${order}&key=${key}`;
 var trendingURL= `${baseURL}&chart=${chart}`;
 
 var trendings;
+var youtube_cats= [];
 
 
 function populatePage(json) {
     var items= json.items;
     items.forEach((item)=>{
-        if (trendings.length>=maxResults) return;
+        if (trendings.length>maxShowResults) return;
         trendings.push(item);
         i++;
         var vid= item.id.videoId;
         var snippet= item.snippet;
         var thumbnails= snippet.thumbnails.medium;
+        var tags= snippet.tags!==undefined? snippet.tags.join(', ') : 'no tags';
         $('#show').append(`
           <article>
             <h3>${i} -> ${snippet.title}</h3>
+            <h5>Cat_id= ${snippet.categoryId}</h5>
+            <h4>Channel Title: ${snippet.channelTitle}</h4>
             <a href='${youtubeBaseURL}${vid}' target='_blank'>
               <img src='${thumbnails.url}' alt='${vid}' style='width:${thumbnails.width};height:${thumbnails.height};'>
             </a>
             <p>Uploaded at ${snippet.publishedAt}</p>
+            <p>${tags}</p>
           </article>
         `);
     });
 }
 
 function ajaxCallMostPopular(nextPage) {
-    var url= `${trendingURL}&order=viewCount`;
+    var url= trendingURL;
     if (nextPage!='') {
         url= `${trendingURL}&pageToken=${nextPage}`;
     } 
@@ -42,28 +48,28 @@ function ajaxCallMostPopular(nextPage) {
         url: url,
         success: function(json) {
             var nextpage= json.nextPageToken;
+            console.log(json);
             populatePage(json);
-            if (trendings.length<maxResults) ajaxCallQuery(nextpage);
+            if (trendings.length<maxShowResults) ajaxCallMostPopular(nextpage);
         },
         error: function(e1,e2,e3) { console.log('e1:',e1,', e2:',e2,', e3:',e3); }
     });
 }
 
 
-function ajaxCallQuery(keyword,nextPage) {
-    if (!(/\S/.test(keyword))) return;
-    var url= `${baseURL}&q=${keyword}&order=relevance`;
-    console.log(keyword.replace(/\s/g,'+'));
+function ajaxCallQuery(cat_id,nextPage) {
+    var url= `${trendingURL}&videoCategoryId=${cat_id}`;
     if (nextPage!='') {
-        url= `${requestURL}&pageToken=${nextPage}`;
+        url= `${url}&pageToken=${nextPage}`;
     }
     $.ajax({
         method: 'GET',
         url: url,
         success: function(json) {
             var nextpage= json.nextPageToken;
+            console.log(json);
             populatePage(json);
-            if (trendings.length<maxResults) ajaxCallMostPopular(nextpage);
+            if (trendings.length<maxShowResults) ajaxCallQuery(cat_id,nextpage);
         },
         error: function(e1,e2,e3) { console.log('e1:',e1,', e2:',e2,', e3:',e3); }
     });
@@ -75,18 +81,35 @@ function clear() {
     i= 0;
 }
 
+function populateCategories() {
+    youtube_categories_json.items.forEach((item)=>{
+        youtube_cats.push({
+            'id': item.id,
+            'title': item.snippet.title,
+        });
+        $('#select_cat').append(`
+          <option value='${item.id}'>${item.snippet.title}</option>
+        `);
+    });
+    $('#select_cat').children().eq(0).attr('selected','selected');
+}
+
 $(document).ready(function(){
 
+    populateCategories();    
+
+    ///*
     $('#btn_trending').on('click',function(){
         clear();
         ajaxCallMostPopular('');
     });
 
     $('#btn_query').on('click',function(){
-        var keywords= $('#input_query').val();
-        if (keywords=='') return;
+        var cat_id= $('#select_cat option:selected').val();
+        //console.log(cat_id); return;
+        if (cat_id=='') return;
         clear();
-        ajaxCallQuery(keywords,'');
+        ajaxCallQuery(cat_id,'');
     });
-
+    //*/
 });
