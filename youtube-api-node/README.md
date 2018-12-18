1aab5400bd909796809d06f49ded58544bbbda6f
kleranscoding <clarencecc88@gmail.com>
2018-12-18 02:12:42 -0600

    removed files and folders

--- youtube-api-node/README.md
+++
@@ -1,3 +0,0 @@
-# YouTube API Demo (using Ajax)
-
-Download the folder and run `./build.sh` in terminal to build the app.
--- youtube-api-node/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm init
-npm install express --save
-npm install body-parser --save
-npm install nodemon --save
-
