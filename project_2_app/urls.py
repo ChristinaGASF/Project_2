fb79ec338b8d3227735a6df3731bf1b078285cb5
kleranscoding <clarencecc88@gmail.com>
2018-12-18 19:15:07 -0600

    populate youtube videos to content page

--- project_2_app/urls.py
+++ project_2_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns= [
     #path('user_signin',views.user_signin,name='user_signin'),
     path('logout/',views.user_logout,name='logout'),
     path('about/',views.about,name='about'),
+    path('youtube/',views.get_youtube,name='youtube'),
     #path('',views.analysis,name='analysis'),
     #path('',views.profile,name='profile'),
     path('content/',views.content,name='content'),
