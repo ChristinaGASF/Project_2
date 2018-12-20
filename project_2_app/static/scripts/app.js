6b1212926954af523e58c9c898f7a0d99da29b11
dogtownoak <david.brandon.ford@gmail.com>
2018-12-19 19:48:45 -0800

    deleted code sample in app.js

--- project_2_app/static/scripts/app.js
+++ project_2_app/static/scripts/app.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
     console.log("jQuery up and running")
 });
-
-
-curl -X GET -u "apikey":"hB8gmTcFeu7MNsKDbRhW8_AYWntJn0sTcEV8A6SGJA7c" \ 
-  "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-11-16&url=www.ibm.com&features=keywords,entities&entities.emotion=true&entities.sentiment=true&keywords.emotion=true&keywords.sentiment=true"
